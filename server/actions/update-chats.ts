591320149153d1a1482d62b54b446a0a78281ae1
hooniee0811 <sinclair0811@kaist.ac.kr>
2024-01-22 22:26:31 +0900

    server complete

--- server/actions/update-chats.ts
+++ server/actions/update-chats.ts
@@ -1,15 +1,28 @@
 import { IN1Building } from "@/types/N1Building";
 import { Command } from "@colyseus/command";
 import { Client, Room } from "colyseus";
+import { Chatting } from "../rooms/schema/RoomState";
 
 type Payload = {
-    client: Client;
-    content: string;
-}
+  client: Client;
+  content: string;
+};
 
 export default class UpdateChats extends Command<Room<IN1Building>, Payload> {
-    execute(data: Payload) {
-        const {client, content} = data
-        const player = this.room.state.players.get(client.sessionId)
-    }
+  //add new chat to the room's chat list
+  execute(data: Payload) {
+    const { client, content } = data;
+    const player = this.room.state.players.get(client.sessionId);
+    const chatMessages = this.room.state.chatMessages;
+
+    if (!chatMessages || !player) return;
+
+    if (chatMessages.length >= 100) chatMessages.shift();
+
+    const newMessage = new Chatting();
+
+    newMessage.user = player.name;
+    newMessage.content = content;
+    chatMessages.push(newMessage);
+  }
 }
---
+++ server/actions/update-whiteboard-user.ts
@@ -0,0 +1,30 @@
+import { IN1Building } from "@/types/N1Building";
+import { Command } from "@colyseus/command";
+import { Client, Room } from "colyseus";
+
+type Payload = {
+  client: Client;
+  whiteboardId: string;
+};
+
+export class WhiteboardAddUser extends Command<Room<IN1Building>, Payload> {
+  execute(data: Payload) {
+    const { client, whiteboardId } = data;
+    const whiteboard = this.room.state.whiteboards.get(whiteboardId);
+    const clientId = client.sessionId;
+
+    if (!whiteboard || whiteboard.connectedUsers.has(clientId)) return;
+    whiteboard.connectedUsers.add(clientId);
+  }
+}
+
+export class WhiteboardRemoveUser extends Command<Room<IN1Building>, Payload> {
+  execute(data: Payload) {
+    const { client, whiteboardId } = data;
+    const whiteboard = this.state.whiteboards.get(whiteboardId);
+
+    if (whiteboard?.connectedUsers.has(client.sessionId)) {
+      whiteboard.connectedUsers.delete(client.sessionId);
+    }
+  }
+}
